b98742f95e9f146a17cbf080fc35d6261653eec7
Jim Crossley <jim@crossleys.org>
2012-09-10 18:21:52 -0400

    lein-release plugin: preparing 0.3.0 release

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.immutant/fntest "0.3.0-SNAPSHOT"
+(defproject org.immutant/fntest "0.3.0"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [jboss-as-management "0.1.2"]]
-  :dev-dependencies [[lein-clojars "0.9.1"]])
+  :lein-release {:deploy-via :clojars})
