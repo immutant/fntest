4201b60259b62bbdd675494c671be7009b9ced2c
Jim Crossley <jim@crossleys.org>
2012-10-28 18:19:56 -0400

    Bump version to 0.3.4-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/fntest "0.3.3"
+(defproject org.immutant/fntest "0.3.4-SNAPSHOT"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
