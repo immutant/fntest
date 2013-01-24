50da3f7e6d29e7e8177c81bbcfd0e53bd8903ec3
Jim Crossley <jim@crossleys.org>
2013-01-24 12:08:07 -0500

    Release 0.3.4.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject org.immutant/fntest "0.3.4-SNAPSHOT"
+(defproject org.immutant/fntest "0.3.4"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [jboss-as-management "0.1.3"]]
+                 [jboss-as-management "0.1.3"]
+                 [org.clojure/tools.nrepl "0.2.1"]]
   :lein-release {:deploy-via :clojars})
