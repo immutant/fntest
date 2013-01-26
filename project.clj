edbbbb6b280702123528cad5b2b9d933f6e3e114
Toby Crawley <toby@tcrawley.org>
2013-01-25 21:12:43 -0500

    Bump to newer jboss-as-management that has newer json dep.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [jboss-as-management "0.1.3"]
+  :dependencies [[jboss-as-management "0.2.0"]
                  [org.clojure/tools.nrepl "0.2.1"]]
   :lein-release {:deploy-via :clojars})
