5644e9507001350eb1aa6e192d40397b82661605
Toby Crawley <toby@tcrawley.org>
2013-10-10 17:08:21 -0400

    The next release has big changes, so...

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/fntest "0.3.16-SNAPSHOT"
+(defproject org.immutant/fntest "0.4.0-SNAPSHOT"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
