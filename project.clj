5a2836a0ca18e28dd4ceb81890ab3394114632d3
Jim Crossley <jim@crossleys.org>
2013-11-09 10:36:36 -0500

    Bumping version to reflect new domain mode support

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.immutant/fntest "0.4.3-SNAPSHOT"
+(defproject org.immutant/fntest "0.5.0-SNAPSHOT"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jboss-as-management "0.3.0-SNAPSHOT"]
+  :dependencies [[jboss-as-management "0.3.0"]
                  [org.clojure/tools.nrepl "0.2.1"]
                  [backtick "0.1.0"]
                  [bultitude "0.2.0"]]
