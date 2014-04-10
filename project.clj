84052549372a911732920c1029baf905e402b8d4
Toby Crawley <toby@tcrawley.org>
2014-04-10 13:13:56 -0400

    Release 0.5.4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/fntest "0.5.4-SNAPSHOT"
+(defproject org.immutant/fntest "0.5.4"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
