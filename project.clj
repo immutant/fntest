84f05ef4a0f5f812677d26481c351e657bf9773f
Toby Crawley <toby@tcrawley.org>
2014-04-10 13:13:42 -0400

    Update to newer bultitude to bring in newer dynapath.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[jboss-as-management "0.3.2"]
                  [org.clojure/tools.nrepl "0.2.1"]
                  [backtick "0.1.0"]
-                 [bultitude "0.2.0"]]
+                 [bultitude "0.2.6"]]
   :profiles {:dev
              {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :signing {:gpg-key "BFC757F9"}
