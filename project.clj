72af0bfdcb126c2e479f3e74cd0090be2e7d11b5
Toby Crawley <toby@tcrawley.org>
2014-04-10 13:14:31 -0400

    Bump version to 0.5.5-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/fntest "0.5.4"
+(defproject org.immutant/fntest "0.5.5-SNAPSHOT"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
