5ad179c4cdb0c29c714ff293b1bac5cd0a30e10a
Jim Crossley <jim@crossleys.org>
2015-03-22 20:06:11 -0400

    Pick up fixes for WF9 HEAD

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jboss-as-management "0.4.0"]
+  :dependencies [[jboss-as-management "0.4.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.immutant/deploy-tools "2.0.0"]
                  [backtick "0.1.0"]
