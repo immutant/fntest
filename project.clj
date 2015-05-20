8e843ce1633f7864248732a2f7cd1e8b0e1937dc
Toby Crawley <toby@tcrawley.org>
2015-05-20 10:48:22 -0400

    Version 2.0.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/fntest "2.0.5"
+(defproject org.immutant/fntest "2.0.6-SNAPSHOT"
   :description "A harness for running Immutant integration tests"
   :url "https://github.com/immutant/fntest"
   :license {:name "Eclipse Public License"
