ebedcaab09a4c8efd0f6e684bce7781faf26958a
Toby Crawley <toby@tcrawley.org>
2015-11-05 12:31:05 -0500

    Use non-SNAPSHOT deploy-tools.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[jboss-as-management "0.4.2"]
                  [org.clojure/tools.nrepl "0.2.3"]
-                 [org.immutant/deploy-tools "2.1.0-SNAPSHOT"]
+                 [org.immutant/deploy-tools "2.1.0"]
                  [backtick "0.1.0"]
                  [bultitude "0.2.6"]]
   :profiles {:dev
